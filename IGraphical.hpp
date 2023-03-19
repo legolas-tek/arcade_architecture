74474fb0abe31d92d24d88b5ff28e605431a214b
jerem <jeremy.elalouf@epitech.eu>
2023-03-19 14:03:26 +0100

    fix: remove const identifier in getEvent()

--- IGraphical.hpp
+++ IGraphical.hpp
@@ -26,7 +26,7 @@ namespace arcd {
             ** @brief Get the event received if there is one.
             ** @return The received event name.
             */
-            virtual arcd::event_t getEvent() const = 0;
+            virtual arcd::event_t getEvent() = 0;
 
             /**
             ** @brief Load the map that is needed to be draw.
