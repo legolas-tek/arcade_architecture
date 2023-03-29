a9bc2d9ea3017190188ed593b9f6f5a41feea22b
yunse-lee-EPITECH <yunse-lee-EPITECH@users.noreply.github.com>
2023-03-29 07:34:14 +0000

    deploy: 81b705ba2b567b180805dd5e85cc81902dde5257

--- search/all_2.js
+++ search/all_2.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['game_5',['Game',['../structarcade_1_1Game.html',1,'arcade']]],
-  ['getevent_6',['getEvent',['../classarcade_1_1IGraphical.html#ada04755153ebf924c7bd9c4a016a486e',1,'arcade::IGraphical']]],
-  ['getgameentity_7',['getGameEntity',['../classarcade_1_1IGame.html#a03fb35256cab01e11cc7d68929052d2c',1,'arcade::IGame']]],
+  ['getevent_6',['getEvent',['../classarcade_1_1IGraphical.html#a231ffe950a4e48b6b135c74b34fa4984',1,'arcade::IGraphical']]],
+  ['getgameentity_7',['getGameEntity',['../classarcade_1_1IGame.html#a3ce1ee76eb87adb0473004d7c129a21b',1,'arcade::IGame']]],
   ['getmapsize_8',['getMapSize',['../classarcade_1_1IGame.html#aa362594dbf2d158283c6fd1bb5d735c1',1,'arcade::IGame']]]
 ];
--- search/functions_0.js
+++ search/functions_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['getevent_32',['getEvent',['../classarcade_1_1IGraphical.html#ada04755153ebf924c7bd9c4a016a486e',1,'arcade::IGraphical']]],
-  ['getgameentity_33',['getGameEntity',['../classarcade_1_1IGame.html#a03fb35256cab01e11cc7d68929052d2c',1,'arcade::IGame']]],
+  ['getevent_32',['getEvent',['../classarcade_1_1IGraphical.html#a231ffe950a4e48b6b135c74b34fa4984',1,'arcade::IGraphical']]],
+  ['getgameentity_33',['getGameEntity',['../classarcade_1_1IGame.html#a3ce1ee76eb87adb0473004d7c129a21b',1,'arcade::IGame']]],
   ['getmapsize_34',['getMapSize',['../classarcade_1_1IGame.html#aa362594dbf2d158283c6fd1bb5d735c1',1,'arcade::IGame']]]
 ];
